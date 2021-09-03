bf68dc8eb311488e86ec3935d53642365bee22bd
Jackson <jackson.mota@zup.com.br>
2021-09-03 16:22:06 -0300

    Correção pós review

--- src/test/java/br/com/zup/library/config/TestConfig.java
+++ src/test/java/br/com/zup/library/config/TestConfig.java
@@ -6,8 +6,14 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-//Classe e método criados por que o Spring agora devolve os JSON no MockMvc com o Encoding errado para acentos, etc.
-//Consultar link: https://stackoverflow.com/questions/58525387/mockmvc-no-longer-handles-utf-8-characters-with-spring-boot-2-2-0-release
+/**
+ * Configuração necessária, pois a partir do Spring Boot v2.2.x o encoding padrão não é mais definido como UTF-8,
+ * mas simplesmente assumido que quando não informado na response o client deve interpretar a response como UTF-8.
+ * Essa mudança parece não impactar em produção (os browsers parecem lidar bem com isso), mas impacta nos
+ * testes de integração, diretamente no MockMvc.
+ *
+ * https://stackoverflow.com/questions/58525387/mockmvc-no-longer-handles-utf-8-characters-with-spring-boot-2-2-0-release
+ */
 @Configuration
 public class TestConfig implements WebMvcConfigurer {
 
