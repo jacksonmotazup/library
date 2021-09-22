0e7e5cb19221889340f068587b0dceb7f38abe02
Jackson <jackson.mota@zup.com.br>
2021-09-22 10:50:59 -0300

    Refatoração

--- src/main/java/br/com/zup/library/emprestimo/EmprestimoController.java
+++ src/main/java/br/com/zup/library/emprestimo/EmprestimoController.java
@@ -1,5 +1,7 @@
 package br.com.zup.library.emprestimo;
 
+import br.com.zup.library.livro.LivroRepository;
+import br.com.zup.library.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,14 +15,19 @@ import javax.validation.Valid;
 public class EmprestimoController {
 
     private final EmprestimoService emprestimoService;
+    private final UsuarioRepository usuarioRepository;
+    private final LivroRepository livroRepository;
 
     @Autowired
-    public EmprestimoController(EmprestimoService emprestimoService) {
+    public EmprestimoController(EmprestimoService emprestimoService, UsuarioRepository usuarioRepository, LivroRepository livroRepository) {
         this.emprestimoService = emprestimoService;
+        this.usuarioRepository = usuarioRepository;
+        this.livroRepository = livroRepository;
     }
 
     @PostMapping
     public NovoEmprestimoResponse cadastra(@Valid @RequestBody NovoEmprestimoRequest request) {
-        return emprestimoService.cadastra(request);
+        var solicitacaoEmprestimo = request.toModel(usuarioRepository, livroRepository);
+        return emprestimoService.realizaEmprestimo(solicitacaoEmprestimo);
     }
 }
--- src/main/java/br/com/zup/library/emprestimo/NovoEmprestimoRequest.java
+++ src/main/java/br/com/zup/library/emprestimo/NovoEmprestimoRequest.java
@@ -1,11 +1,23 @@
 package br.com.zup.library.emprestimo;
 
+import br.com.zup.library.livro.LivroRepository;
+import br.com.zup.library.usuario.TipoUsuario;
+import br.com.zup.library.usuario.UsuarioRepository;
 import org.hibernate.validator.constraints.Range;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+import static br.com.zup.library.usuario.TipoUsuario.PADRAO;
+import static br.com.zup.library.usuario.TipoUsuario.PESQUISADOR;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
 public class NovoEmprestimoRequest {
+
+    public final Integer PRAZO_MAXIMO = 60;
 
     @NotNull
     private final Long idLivro;
@@ -30,5 +42,25 @@ public class NovoEmprestimoRequest {
 
     public Integer getPrazoDevolucao() {
         return prazoDevolucao;
+    }
+
+    public SolicitacaoEmprestimo toModel(UsuarioRepository usuarioRepository, LivroRepository livroRepository) {
+        var usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Usuário não encontrado"));
+
+        var livro = livroRepository.findById(idLivro)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Livro não encontrado"));
+
+        var prazoDevolucaoFinal = retornaPrazoDevolucao(usuario.getTipoUsuario());
+
+        return new SolicitacaoEmprestimo(usuario, livro, prazoDevolucaoFinal);
+    }
+
+    private Integer retornaPrazoDevolucao(TipoUsuario tipoUsuario) {
+        if (PADRAO.equals(tipoUsuario) && Objects.isNull(prazoDevolucao)) {
+            throw new ResponseStatusException(BAD_REQUEST, "Prazo deve ser preenchido");
+        } else if (PESQUISADOR.equals(tipoUsuario) && Objects.isNull(prazoDevolucao)) {
+            return PRAZO_MAXIMO;
+        } else return prazoDevolucao;
     }
 }
